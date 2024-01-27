98a09f43a63526d920201c51cf4c307a0e1ce4cf
mmmiz <miduho4soft@yahoo.co.jp>
2024-01-27 10:03:14 -0600

    updated

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ mongoose.connect(process.env.MONGO_DB_URL
 });
 
 app.use(cors({
-  origin: 'http://localhost:3001',
-  // origin: 'https://mycolors-new.onrender.com',
+  // origin: 'http://localhost:3001',
+  origin: 'https://new-mycolors.onrender.com',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 }));
